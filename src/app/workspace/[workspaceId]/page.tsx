0b4e824066296043d5f0a7fb3c7a3ab959dfd002
Ali <ali.arioglu06@gmail.com>
2024-08-27 22:47:48 +0300

    sidebar

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-
 const WorkspaceIdPage = () => {
-  const workspaceId = useWorkspaceId();
-  const { data } = useGetWorkspace({ id: workspaceId });
-
-  return <div>Data: {JSON.stringify(data)}</div>;
+  return <div>WorkspaceId Page</div>;
 };
 
 export default WorkspaceIdPage;
