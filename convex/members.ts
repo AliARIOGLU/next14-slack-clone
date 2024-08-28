d0544873b19b66e190d0102c82b82af31296eaad
Ali <ali.arioglu06@gmail.com>
2024-08-28 14:31:47 +0300

    workspace sidebar

---
+++ convex/members.ts
@@ -0,0 +1,26 @@
+import { v } from "convex/values";
+
+import { auth } from "./auth";
+import { query } from "./_generated/server";
+
+export const current = query({
+  args: { workspaceId: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      return null;
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) return null;
+
+    return member;
+  },
+});
